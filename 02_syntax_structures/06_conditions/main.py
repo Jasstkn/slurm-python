2033c0329d8552f8a12dd8569fda4fdc7cff3be6
Maria Kotlyarevskaya <jasssstkn@yahoo.com>
2021-06-08 23:01:42 +0300

    Submit conditions task
    Signed-off-by: Maria Kotlyarevskaya <jasssstkn@yahoo.com>

---
+++ 02_syntax_structures/06_conditions/main.py
@@ -0,0 +1,16 @@
+# TODO: В ветке <название_ветки> непроверенный код, пропускаем
+
+branch = input()
+tests_passed = int(input())
+coverage = float(input())
+approvals = int(input())
+
+if branch.lower() not in ("development", "staging"):
+    print(f"В ветке {branch} непроверенный код, пропускаем")
+else:
+    if tests_passed == 1 and coverage > 5:
+        print(f"Внимание! Код из {branch} отправлен в релиз!")
+    elif tests_passed == 1 and approvals >= 3:
+        print(f"Внимание! Код из {branch} отправлен в релиз!")
+    else:
+        print(f"Код из {branch} с результатами тесты: {tests_passed}, coverage: {coverage}, approve: {approvals} в релиз не попадает.")
